90636d08050baf82bf8e6c259c6f95a9554acd70
Simon Bull <simon.bull.84@googlemail.com>
2020-09-10 14:01:35 +0100

    Update for latest registry structure

--- HistoricalDataPreparer/src/Dfe.Spi.HistoricalDataPreparer.Infrastructure.FileSystem/Registry/FileSystemPreparedRegistryRepository.cs
+++ HistoricalDataPreparer/src/Dfe.Spi.HistoricalDataPreparer.Infrastructure.FileSystem/Registry/FileSystemPreparedRegistryRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Dfe.Spi.HistoricalDataPreparer.Domain.Registry;
@@ -38,10 +39,12 @@ namespace Dfe.Spi.HistoricalDataPreparer.Infrastructure.FileSystem.Registry
 
         public async Task StoreRegisteredEntity(RegisteredEntity entity, DateTime date, CancellationToken cancellationToken)
         {
-            var json = JsonConvert.SerializeObject(entity);
-            await FileHelper.WriteStringToFileAsync(Path.Combine(_dataDirectory, $"{entity.Id}.json"), json);
+            var storableEntity = Map(entity);
+            
+            var json = JsonConvert.SerializeObject(storableEntity);
+            await FileHelper.WriteStringToFileAsync(Path.Combine(_dataDirectory, $"{storableEntity.Id}.json"), json);
 
-            await _index.AddRegisteredEntityIdAsync(entity, entity.ValidFrom, cancellationToken);
+            await _index.AddRegisteredEntityIdAsync(storableEntity, storableEntity.ValidFrom, cancellationToken);
         }
 
         public async Task DeleteRegisteredEntity(string id, CancellationToken cancellationToken)
@@ -53,6 +56,54 @@ namespace Dfe.Spi.HistoricalDataPreparer.Infrastructure.FileSystem.Registry
             }
 
             await _index.DeleteRegisteredEntityIdAsync(id, cancellationToken);
+        }
+        
+        
+        
+        private CosmosRegisteredEntity Map(RegisteredEntity registeredEntity)
+        {
+            var partitionableId = registeredEntity.Entities.FirstOrDefault(e => e.Urn.HasValue)?.Urn?.ToString();
+            if (string.IsNullOrEmpty(partitionableId))
+            {
+                partitionableId = registeredEntity.Entities.FirstOrDefault(e => e.Ukprn.HasValue)?.Ukprn?.ToString();
+            }
+
+            if (string.IsNullOrEmpty(partitionableId))
+            {
+                partitionableId = registeredEntity.Entities.FirstOrDefault(e => !string.IsNullOrEmpty(e.ManagementGroupCode))?.ManagementGroupCode;
+            }
+
+            return new CosmosRegisteredEntity
+            {
+                Id = registeredEntity.Id,
+                Type = registeredEntity.Type,
+                ValidFrom = registeredEntity.ValidFrom,
+                ValidTo = registeredEntity.ValidTo,
+                Entities = registeredEntity.Entities,
+                Links = registeredEntity.Links,
+
+                PartitionableId = partitionableId,
+                SearchableSourceSystemIdentifiers = registeredEntity.GetSearchableValues(e => $"{e.SourceSystemName}:{e.SourceSystemId}"),
+                SearchableName = registeredEntity.GetSearchableValues(e => e.Name),
+                SearchableType = registeredEntity.GetSearchableValues(e => e.Type),
+                SearchableSubType = registeredEntity.GetSearchableValues(e => e.SubType),
+                SearchableStatus = registeredEntity.GetSearchableValues(e => e.Status),
+                SearchableOpenDate = registeredEntity.GetSearchableValues(e => e.OpenDate),
+                SearchableCloseDate = registeredEntity.GetSearchableValues(e => e.CloseDate),
+                SearchableUrn = registeredEntity.GetSearchableValues(e => e.Urn),
+                SearchableUkprn = registeredEntity.GetSearchableValues(e => e.Ukprn),
+                SearchableUprn = registeredEntity.GetSearchableValues(e => e.Uprn),
+                SearchableCompaniesHouseNumber = registeredEntity.GetSearchableValues(e => e.CompaniesHouseNumber),
+                SearchableCharitiesCommissionNumber = registeredEntity.GetSearchableValues(e => e.CharitiesCommissionNumber),
+                SearchableAcademyTrustCode = registeredEntity.GetSearchableValues(e => e.AcademyTrustCode),
+                SearchableDfeNumber = registeredEntity.GetSearchableValues(e => e.DfeNumber),
+                SearchableLocalAuthorityCode = registeredEntity.GetSearchableValues(e => e.LocalAuthorityCode),
+                SearchableManagementGroupType = registeredEntity.GetSearchableValues(e => e.ManagementGroupType),
+                SearchableManagementGroupId = registeredEntity.GetSearchableValues(e => e.ManagementGroupId),
+                SearchableManagementGroupCode = registeredEntity.GetSearchableValues(e => e.ManagementGroupCode),
+                SearchableManagementGroupUkprn = registeredEntity.GetSearchableValues(e => e.ManagementGroupUkprn),
+                SearchableManagementGroupCompaniesHouseNumber = registeredEntity.GetSearchableValues(e => e.ManagementGroupCompaniesHouseNumber),
+            };
         }
     }
 }
